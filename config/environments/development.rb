7158ae319b5dc132936eaeab378487cc8b9c4192
anastasiabalaban <anastasiabalaban07@gmail.com>
2018-04-13 22:31:08 +0300

    add images

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,11 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025} 
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+
+  config.action_controller.asset_host = 'http://localhost:3000'
+  config.action_mailer.asset_host = config.action_controller.asset_host
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
