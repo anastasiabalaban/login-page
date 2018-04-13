9690d9fe56f56d4541045a6e51d7cbedd1c3208c
anastasiabalaban <anastasiabalaban07@gmail.com>
2018-04-13 23:25:11 +0300

    add configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,9 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
-  # config.action_mailer.asset_host = 'https://quiet-coast-53432.herokuapp.com'
-  config.serve_static_assets = true
+  config.action_mailer.asset_host = 'https://quiet-coast-53432.herokuapp.com'
+  config.action_mailer.asset_host = 'https://quiet-coast-53432.herokuapp.com'
+  # config.serve_static_assets = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -31,7 +32,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
